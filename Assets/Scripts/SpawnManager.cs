5d40b2a40791e0d961f995ad91b591112d3d7e24
DiscardedDev <60794490+DiscardedDev@users.noreply.github.com>
2024-09-23 22:00:30 +0100

    SpawnManager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] GameObject[] animals;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.K)){
+            SpawnRandomAnimal();
+        }
+    }
+
+    void SpawnRandomAnimal(){
+        int index = Random.Range(0, animals.Length);
+        Instantiate(animals[index], new(Mathf.Floor(Random.Range(-12,12)),0,30), animals[index].transform.rotation);
+        Debug.Log($"Spawning {animals[index].name}");
     }
 }
